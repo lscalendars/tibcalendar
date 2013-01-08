1a76e4869cc9efed33f2fa1de6575e121666f5ef
Elie Roux <elie.roux@telecom-bretagne.eu>
2013-01-08 17:42:03 +0100

    oops, forgotten in last commit

---
+++ libtastro/tbstructures.c
@@ -0,0 +1,92 @@
+#include <string.h>		// for memset
+#include <stdlib.h> // for malloc, free
+#include "tbstructures.h"
+
+tib_month *
+new_tib_month()
+{
+  tib_month *tm = malloc (sizeof (tib_month));
+  memset (tm, 0, sizeof (tib_month));
+  tm->year = new_tib_year();
+  return tm;
+}
+
+tib_year *
+new_tib_year()
+{
+  tib_year *ty = malloc (sizeof (tib_year));
+  memset (ty, 0, sizeof (tib_year));
+  return ty;
+}
+
+tib_year_astro *
+new_tib_year_astro()
+{
+  tib_year_astro *tya = malloc (sizeof (tib_year_astro));
+  memset (tya, 0, sizeof (tib_year_astro));
+  return tya;
+}
+
+tib_month_astro *
+new_tib_month_astro()
+{
+  tib_month_astro *tma = malloc (sizeof (tib_month_astro));
+  memset (tma, 0, sizeof (tib_month_astro));
+  return tma;
+}
+
+tib_day_astro *
+new_tib_day_astro()
+{
+  tib_day_astro *tda = malloc (sizeof (tib_day_astro));
+  memset (tda, 0, sizeof (tib_day_astro));
+  return tda;
+}
+
+tib_day *
+new_tib_day()
+{
+  tib_day *td = malloc (sizeof (tib_day));
+  memset (td, 0, sizeof (tib_day));
+  td->month = new_tib_month();
+  return td;
+}
+
+tib_planet_data *
+new_tib_planet_data()
+{
+  tib_planet_data *pd = malloc (sizeof (tib_planet_data));
+  memset (pd, 0, sizeof (tib_planet_data));
+  return pd;
+}
+
+// frees the entire tib_year
+void
+free_tib_year(tib_year *ty)
+{
+  if (ty->astro_data)
+    free(ty->astro_data);
+  free(ty);
+}
+
+// frees the entire tib_month (with year)
+void
+free_tib_month(tib_month *tm)
+{
+  free_tib_year(tm->year);
+  if (tm->astro_data)
+    free(tm->astro_data);
+  free(tm);
+}
+
+// frees the entire tib_day (with month and year)
+void
+free_tib_day(tib_day *td)
+{
+  free_tib_month(td->month);
+  if (td->astro_data)
+    free(td->astro_data);
+  if (td->planet_data)
+    free(td->planet_data);
+  free(td);
+}
