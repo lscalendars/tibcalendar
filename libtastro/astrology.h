006362509061f2c9edf295cc4fad2024821d5ff8
Elie Roux <elie.roux@telecom-bretagne.eu>
2013-01-09 15:59:56 +0100

    Fixing error in previous commit

--- libtastro/astrology.h
+++ libtastro/astrology.h
@@ -9,7 +9,7 @@ void get_year_astro_data(tib_year *ty);
 void get_month_astro_data(tib_month *tm, astro_system *asys);
 void get_day_astro_data(tib_day *td, astro_system *asys);
 void check_sadag (unsigned char m, unsigned char t, tib_day_astro_data *tda);
-void check_anniversary (unsigned char m, unsigned char t, tib_day_astro_data *tda, astro_system *asys );
+void check_anniversary (unsigned char m, unsigned char t, unsigned char month_type, tib_day_astro_data *tda, astro_system *asys );
 
 // The four elements
 #define FOUR_EARTH 0
